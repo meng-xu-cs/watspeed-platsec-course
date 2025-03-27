c5f2eab5d13c6710f4565752e471f9abeaf538d6
MumenAlJarah <113619552+MumenJarrah@users.noreply.github.com>
2025-03-27 14:33:18 -0400

    Create sandbox1.c

---
+++ lab4/files/sandbox1.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <seccomp.h>
+#include <linux/seccomp.h>
+#include <syscall.h>
+#include <stdlib.h>
+
+int main() {
+	scmp_filter_ctx ctx;
+	ctx = seccomp_init(SCMP_ACT_ALLOW);
+	seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(write), 0);
+	seccomp_load(ctx);
+
+	char *args[] = {"./sploit1", NULL};
+	execve(args[0], args, NULL);
+}
